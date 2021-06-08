a68b7ccc3aaf315597a50a80976613dbdc20e393
akoroteev <akoroteev@justcoded.co>
2021-06-08 15:20:27 +0300

    testimonials slider

---
+++ docs/.vuepress/components/testimonials-slider.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
+
+
+export default {
+name: "testimonials-slider",
+  components: {
+    swiper,
+    swiperSlide,
+  },
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        // Some Swiper option/callback...
+      }
+    }
+  },
+}
+</script>
---
+++ docs/.vuepress/components/testimonials.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    <testimonialsSlider></testimonialsSlider>
+  </div>
+</template>
+
+<script>
+import testimonialsSlider from './testimonials-slider.vue'
+export default {
+name: "testimonials",
+  components: {
+    testimonialsSlider,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
