82e0e11ee3cf2499241082666806668ba6868933
akoroteev <akoroteev@justcoded.co>
2021-06-08 11:14:59 +0300

    added styles and responsive for use case cards

--- docs/.vuepress/components/use-case-card.vue
+++ docs/.vuepress/components/use-case-card.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="use-case-card">
+  <router-link
+      :to="cardItem.link"
+      class="use-case-card">
     <div class="use-case-card__header">
       <div class="use-case-card__icon">
-        <div class="icon-holder">
-          <img
-              v-if="cardItem.icon"
-              :src="cardItem.icon"
-              alt="use case icon">
-        </div>
+        <img
+            v-if="cardItem.icon"
+            :src="cardItem.icon"
+            alt="use case icon">
       </div>
       <h5 class="use-case-card__title">{{ cardItem.title }}</h5>
     </div>
@@ -16,7 +16,12 @@
         <p> {{cardItem.description}} </p>
       </div>
     </div>
-  </div>
+    <div class="use-case-card__footer">
+      <svg width="18" height="15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.5 0L9.428 1.045l5.684 5.705H0v1.5h15.113l-5.685 5.68L10.5 15 18 7.5 10.5 0z" fill="#242529"/>
+      </svg>
+    </div>
+  </router-link>
 </template>
 
 <script>
