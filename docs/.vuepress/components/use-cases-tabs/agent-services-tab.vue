a35a7280cc1255bb6d414830f51bb255da984d5d
ssidorenkoJustcoded <ssidorenko@justcoded.co>
2021-06-11 15:20:23 +0300

    игпашч

--- docs/.vuepress/components/use-cases-tabs/agent-services-tab.vue
+++ docs/.vuepress/components/use-cases-tabs/agent-services-tab.vue
@@ -9,7 +9,7 @@
         <p><b>Payee-initiated merchant payment.</b> The merchant initiates the payment and the payer is requested to authenticate to confirm acceptance by the mobile money provider. <br>
         <b>Payer-initiated merchant payment.</b> The payer initiates the payment by specifying the merchant that is to be paid.
         <b>Merchant payment via pre-authorised payment code.</b> The payer generates a payment authorisation code up to a maximum payment amount. The merchant then enters or scans (if rendered as a QR code) the payer’s code to perform the payment.</p>
-        <router-link :to="'#'" class="btn btn--transparent">
+        <router-link class="btn btn--transparent" :to="'#'">
           Start developing
         </router-link>
       </div>
