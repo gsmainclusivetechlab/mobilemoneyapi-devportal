76a2dda35d8f335e67f0db6ff6bc65ec4e69ee69
akoroteev <akoroteev@justcoded.co>
2021-06-29 13:02:07 +0300

    added contact form with validation

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,12 +4,23 @@
  * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
 import './styles/scss/styles.scss';
+import Vue from 'vue';
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+import VeeValidate from 'vee-validate';
+// import { ValidationProvider } from 'vee-validate';
+
+Vue.component("v-select", vSelect);
+// Vue.component('ValidationProvider', ValidationProvider);
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  siteData, // site metadata
+
 }) => {
+  Vue.use(VeeValidate);
   // ...apply enhancements for the site.
+  // prevent ReferenceError: window is not defined during production build
 }
