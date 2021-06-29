0495330d347dcc2381e1b6bb08a35afe1296ae6a
akoroteev <akoroteev@justcoded.co>
2021-06-29 16:20:05 +0300

    removed one send mail function

--- docs/.vuepress/dist/form/sendmail.php
+++ docs/.vuepress/dist/form/sendmail.php
@@ -23,11 +23,6 @@ $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 $mail->send();
-
-
-
-
-$mail->send();
 echo 'Message has been sent';
 } catch (Exception $e) {
 echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
