790a87514f9736540ca9812ac24e158ceed7a4bb
akoroteev <akoroteev@justcoded.co>
2021-06-29 17:40:44 +0300

    added new message

--- docs/.vuepress/dist/form/sendmail.php
+++ docs/.vuepress/dist/form/sendmail.php
@@ -1,7 +1,35 @@
 <?php
-$to_email = 'akoroteeev@justcoded.co';
-$subject = 'Testing PHP Mail';
-$message = 'This mail is sent using the PHP mail function';
-$headers = 'From: artyom.koroteev@gmail.com';
-mail($to_email,$subject,$message,$headers);
-?>
+
+/**
+ * This example shows sending a message using PHP's mail() function.
+ */
+
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+
+require '../vendor/autoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+//Set who the message is to be sent from
+$mail->setFrom('from@example.com', 'First Last');
+//Set an alternative reply-to address
+$mail->addReplyTo('artyom.akoroteev@gmail.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('akoroteev@justcoded.co', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer mail() test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+}
