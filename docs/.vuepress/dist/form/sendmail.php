69024adee8de9d061522b685eb5d8f1874f72e66
akoroteev <akoroteev@justcoded.co>
2021-06-29 17:49:07 +0300

    added new script for emails

--- docs/.vuepress/dist/form/sendmail.php
+++ docs/.vuepress/dist/form/sendmail.php
@@ -1,35 +1,29 @@
 <?php
-
-/**
- * This example shows sending a message using PHP's mail() function.
- */
-
-//Import the PHPMailer class into the global namespace
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer();
-//Set who the message is to be sent from
-$mail->setFrom('from@example.com', 'First Last');
-//Set an alternative reply-to address
-$mail->addReplyTo('artyom.akoroteev@gmail.com', 'First Last');
-//Set who the message is to be sent to
-$mail->addAddress('akoroteev@justcoded.co', 'John Doe');
-//Set the subject line
-$mail->Subject = 'PHPMailer mail() test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-//send the message, check for errors
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
+try {
+//Recipients
+$mail->setFrom('artyom.koroteev@gmail.com', 'test');
+$mail->addAddress('akoroteev@justcoded.co');
+$mail->CharSet = 'UTF-8';
+
+//Content
+$mail->isHTML(true);                                  //Set email format to HTML
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+echo 'Message has been sent';
+} catch (Exception $e) {
+echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
