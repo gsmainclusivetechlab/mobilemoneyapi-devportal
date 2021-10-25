3b469e336a37be96453efc49de6cea4715782f52
Andrey Porodnov <aporodnov@justcoded.co>
2021-10-25 09:33:58 +0300

    created component faq-item

--- docs/.vuepress/components/faq-page.vue
+++ docs/.vuepress/components/faq-page.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="faq">
     <search-section
-      @on-search="applyFilter">
+        @on-search="applyFilter">
       <template #title>
         <slot name="title"></slot>
       </template>
@@ -11,22 +11,11 @@
     </search-section>
     <div class="search-results-section">
       <div class="container container--narrow">
-        <h2 v-if="!Object.keys(filteredDaqData).length" class="no-results text-center">No results found.</h2>
+        <h2 v-if="!Object.keys(filteredDaqData).length" class="no-results text-center">No results
+          found.</h2>
         <template v-else>
-          <div class="faq-results__item" v-for="(item, key) in filteredDaqData" :key="`category-${key}`">
-            <h2 class="h2 faq-results__item-title">{{ key }}</h2>
-            <accordion>
-              <accordion-item v-for="(categoryItem, index) in item" :key="`category-item-${index}`">
-                <template #header>
-                  {{ categoryItem.title }}
-                </template>
-
-                <template #body>
-                  <div v-html="categoryItem.text"></div>
-                </template>
-              </accordion-item>
-            </accordion>
-          </div>
+          <faq-item v-for="(item, key) in filteredDaqData" :key="`category-${key}`" :item="item"
+                    :itemKey="key"/>
         </template>
       </div>
     </div>
@@ -34,18 +23,16 @@
 </template>
 
 <script>
-import accordion from './simple-accordion/accordion.vue';
-import accordionItem from './simple-accordion/accordion-item.vue';
 
 import searchSection from './search-section.vue';
 
 import faqData from '../data/faqData';
+import FaqItem from "./faq-item";
 
 export default {
   name: 'faq-page',
   components: {
-    accordion,
-    accordionItem,
+    FaqItem,
     searchSection,
   },
 
@@ -64,7 +51,6 @@ export default {
   created() {
     this.faqDataSource = faqData;
     this.applyFilter('');
-    console.log(this.$slots)
   },
 
   methods: {
@@ -74,9 +60,9 @@ export default {
       this.faqDataSource.forEach(item => {
         item.items.forEach(el => {
           if (
-            el.title.toLowerCase().includes(searchebleValue.toLowerCase())
-            ||
-            el.text.toLowerCase().includes(searchebleValue.toLowerCase())
+              el.title.toLowerCase().includes(searchebleValue.toLowerCase())
+              ||
+              el.text.toLowerCase().includes(searchebleValue.toLowerCase())
           ) {
             if (!this.filteredDaqData[item.title]) {
               this.filteredDaqData[item.title] = [];
