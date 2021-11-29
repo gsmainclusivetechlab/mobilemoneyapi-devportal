90c5396ceb6bd1e409568579bfba72463056fdf1
Andrey Porodnov <aporodnov@justcoded.co>
2021-11-29 12:16:22 +0200

    made refactoring of dashboard-section

---
+++ docs/.vuepress/components/dashboard-table/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="dashboard-content dashboard-content__table" :class="[tableClass]">
+    <h3>{{ tableTitle }}</h3>
+    <div class="table-block">
+      <dashboard-table-top :hideFilter="hideFilter"/>
+      <table class="dashboard-table">
+        <tr class="dashboard-table__row">
+          <th class="dashboard-table__cell dashboard-table__cell--heading"
+              :class="{'dashboard-table__cell--center': isCenterHeader ? index === indexCenter : false}"
+              v-for="(title, index) of tableHeadersData"
+              :key="index">
+            {{ title }}
+          </th>
+          <th class="dashboard-table__cell dashboard-table__cell--heading">
+          </th>
+        </tr>
+        <slot></slot>
+      </table>
+      <dashboard-table-bottom/>
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardTableBottom from "./dashboard-table-bottom";
+import DashboardTableTop from "./dashboard-table-top";
+export default {
+  name: "dashboard-table",
+  components: {DashboardTableTop, DashboardTableBottom},
+  props: {
+    tableHeadersData: {
+      type: Array,
+      default: () => ([])
+    },
+    isCenterHeader: {
+      type: Boolean,
+      default: false
+    },
+    indexCenter: {
+      type: Number,
+      default: 0
+    },
+    tableTitle: {
+      type: String,
+      default: ''
+    },
+    tableClass: {
+      type: String,
+      default: ''
+    },
+    hideFilter: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- docs/.vuepress/components/dashboard/plans-tab.vue
+++ docs/.vuepress/components/dashboard/plans-tab.vue
@@ -1,52 +1,43 @@
 <template>
-  <div class="dashboard-content dashboard-content__table dashboard-content__table-plans">
-    <h3>All plans</h3>
-    <div class="table-block">
-      <dashboard-table-top hide-filter/>
-      <table class="dashboard-table">
-        <tr class="dashboard-table__row">
-          <th class="dashboard-table__cell dashboard-table__cell--heading" :class="{'dashboard-table__cell--center': index === 1}" v-for="(title, index) of allPlansHeaderTitles" :key="index">
-            {{ title }}
-          </th>
-          <th class="dashboard-table__cell dashboard-table__cell--heading">
-          </th>
-        </tr>
-        <tr class="dashboard-table__row" v-for="plan of plans" :key="plan.id">
-          <td class="dashboard-table__cell">
-            {{ plan.planName }}
-          </td>
-          <td class="dashboard-table__cell dashboard-table__cell--center dashboard-table__cell--state" >
-            <span class="dashboard-table__state-label" :class="[getPlanStatusLabelClass(plan.state)]">{{ getPlanStatus(plan.state) }}</span>
-          </td>
-          <td class="dashboard-table__cell dashboard-table__cell--options">
-            <button type="button" class="dashboard-table__button" @click="showUserOptions(plan.id)">
-              <svg width="2" height="10" viewBox="0 0 2 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="1" cy="1" r="1" transform="rotate(90 1 1)" fill="#7C7C7F"/>
-                <circle cx="1" cy="5" r="1" transform="rotate(90 1 5)" fill="#7C7C7F"/>
-                <circle cx="1" cy="9" r="1" transform="rotate(90 1 9)" fill="#7C7C7F"/>
-              </svg>
-            </button>
-            <user-options-block v-on-clickaway="hideUserOptions" v-if="plan.id === activeOptionsPlanId"/>
-          </td>
-        </tr>
-      </table>
-      <dashboard-table-bottom/>
-    </div>
-  </div>
+  <dashboard-table
+      is-center-header
+      hide-filter
+      table-title="All plans"
+      table-class="dashboard-content__table-plans"
+      :indexCenter="1"
+      :table-headers-data="allPlansHeaderTitles">
+    <tr class="dashboard-table__row" v-for="plan of plans" :key="plan.id">
+      <td class="dashboard-table__cell">
+        {{ plan.planName }}
+      </td>
+      <td class="dashboard-table__cell dashboard-table__cell--center dashboard-table__cell--state">
+        <span class="dashboard-table__state-label" :class="[getPlanStatusLabelClass(plan.state)]">{{ getPlanStatus(plan.state) }}</span>
+      </td>
+      <td class="dashboard-table__cell dashboard-table__cell--options">
+        <button type="button" class="dashboard-table__button" @click="showUserOptions(plan.id)">
+          <svg width="2" height="10" viewBox="0 0 2 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="1" cy="1" r="1" transform="rotate(90 1 1)" fill="#7C7C7F"/>
+            <circle cx="1" cy="5" r="1" transform="rotate(90 1 5)" fill="#7C7C7F"/>
+            <circle cx="1" cy="9" r="1" transform="rotate(90 1 9)" fill="#7C7C7F"/>
+          </svg>
+        </button>
+        <user-options-block v-on-clickaway="hideUserOptions" v-if="plan.id === activeOptionsPlanId"/>
+      </td>
+    </tr>
+  </dashboard-table>
 </template>
 
 <script>
 import {allPlansHeaderTitles} from "../../constants";
 import plans from '../../api/mocks/plans.json';
 import UserOptionsBlock from "../user-options-block";
-import { mixin as clickaway } from 'vue-clickaway';
-import DashboardTableTop from "../dashboard-table/dashboard-table-top";
-import DashboardTableBottom from "../dashboard-table/dashboard-table-bottom";
+import {mixin as clickaway} from 'vue-clickaway';
+import DashboardTable from "../dashboard-table";
 
 export default {
   name: "plans-tab",
 
-  components: {DashboardTableBottom, DashboardTableTop, UserOptionsBlock},
+  components: {DashboardTable, UserOptionsBlock},
 
   data() {
     return {
