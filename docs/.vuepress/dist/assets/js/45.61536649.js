90fdc47ef9a52faadddfae831d2000b677715f19
Andrey Porodnov <aporodnov@justcoded.co>
2021-12-21 15:46:15 +0200

    make build

--- docs/.vuepress/dist/assets/js/45.61536649.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[45],{469:function(n,t,e){"use strict";e.r(t);var s={name:"DivBox"},i=e(15),c=Object(i.a)(s,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  Test Content\n")])}),[],!1,null,"27f5bbfc",null);t.default=c.exports}}]);
