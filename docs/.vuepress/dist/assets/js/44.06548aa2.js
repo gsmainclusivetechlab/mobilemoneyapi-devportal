89f77be111238ad74e8dc00d0c18e60bd915e961
Andrey Porodnov <aporodnov@justcoded.co>
2021-12-21 15:40:10 +0200

    Revert "made build"
    This reverts commit 49230f0dd9638ec27af49f9c070f758d882e55b2.

---
+++ docs/.vuepress/dist/assets/js/44.06548aa2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{469:function(n,t,e){"use strict";e.r(t);var s={name:"DivBox"},i=e(15),c=Object(i.a)(s,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  Test Content\n")])}),[],!1,null,"27f5bbfc",null);t.default=c.exports}}]);
