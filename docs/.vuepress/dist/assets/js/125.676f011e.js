b214a7dfde63c1e7e6da30d0337698fb12510321
Andrey Porodnov <aporodnov@justcoded.co>
2022-01-06 15:54:11 +0200

    Revert "made build"
    This reverts commit 8cded8b9f82cad85eb538c9ab782cdef29dd3ad0.

--- docs/.vuepress/dist/assets/js/125.676f011e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[125],{591:function(t,e,s){"use strict";s.r(e);var n=s(20),o=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("set-new-password")],1)}),[],!1,null,null,null);e.default=o.exports}}]);
