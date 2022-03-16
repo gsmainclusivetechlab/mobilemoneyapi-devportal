de86b2c776d7cefcc3f94af984c4326793404f40
Andrey Porodnov <aporodnov@justcoded.co>
2022-03-16 09:43:29 +0200

    made build

---
+++ docs/.vuepress/dist/assets/js/87.afd89f46.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[87],{552:function(t,n,o){"use strict";o.r(n);var s=o(24),e=Object(s.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("forgot-password-confirmation")],1)}),[],!1,null,null,null);n.default=e.exports}}]);
